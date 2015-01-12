1bb62dc4c6bcb1bf468f4042aba366c750742c11
iourigordon <iouri.gordon@gmail.com>
2015-01-12 13:57:44 -0800

    added driver registration

--- scull.c
+++ scull.c
@@ -9,10 +9,29 @@
 
 dev_t dev;
 
-static int scull_major = SCULL_MAJOR;
-static int scull_minor = SCULL_MINOR;
-static int scull_nr_devs = SCULL_NR_DEVS;
+int scull_major     = SCULL_MAJOR;
+int scull_minor     = SCULL_MINOR;
+int scull_nr_devs   = SCULL_NR_DEVS;
+int scull_quantum   = SCULL_QUANTUM;
+int scull_qset      = SCULL_QSET;
+
 static int result;
+
+struct scull_dev *scull_devices;	/* allocated in scull_init_module */
+
+static void scull_setup_cdev(struct scull_dev *dev, int index)
+{
+    int err,devno = MKDEV(scull_major,scull_minor + index);
+
+    cdev_init(&dev->cdev,&scull_fops);
+    dev->cdev.owner = THIS_MODULE;
+    dev->cdev.ops   = &scull_fops;
+    err = cdev_add(&dev->cdev,devno,1);
+
+    /* Fail gracefully if need be */
+    if (err)
+        printk(KERN_NOTICE,"Error %d adding scull %d",err,index);
+}
 
 static int __init scull_init(void)
 {
@@ -33,15 +52,44 @@ static int __init scull_init(void)
     if (result < 0)
     {
         printk(KERN_WARNING "scull: can't get major %d\n",scull_major);
+        return result;
     }
 
+    /* 
+	* allocate the devices -- we can't have them static, as the number
+	* can be specified at load time
+	*/
+	scull_devices = kmalloc(scull_nr_devs * sizeof(struct scull_dev), GFP_KERNEL);
+	if (!scull_devices) {
+		result = -ENOMEM;
+		goto fail;  /* Make this more graceful */
+	}
+	memset(scull_devices, 0, scull_nr_devs * sizeof(struct scull_dev));
+
+    /* Initialize each device. */
+	for (i = 0; i < scull_nr_devs; i++) {
+		scull_devices[i].quantum = scull_quantum;
+		scull_devices[i].qset = scull_qset;
+		init_MUTEX(&scull_devices[i].sem);
+		scull_setup_cdev(&scull_devices[i], i);
+	}
+
     return 0;
+
+    fail:
+	    scull_cleanup_module();
+	    return result;
+
 }
 module_init(scull_init);
 
 static void __exit scull_cleanup(void)
 {
     printk(KERN_INFO "scull module exit");
+
+	/* cleanup_module is never called if registering failed */
+	unregister_chrdev_region(devno, scull_nr_devs);
+
 }
 module_exit(scull_cleanup);
 
--- scull.h
+++ scull.h
@@ -1,7 +1,31 @@
 #ifndef __SCULL_H__
 #define __SCULL_H__
 
+struct scull_qset {
+    void **data;
+    struct scull_qset *next;
+};
+
+struct scull_dev {
+    struct scull_qset *data;    /* Pointer to first quantum set */
+    int quantum;                /* The current quantum size */
+    int qset;                   /* The current array size */
+    unsinged long size;         /* amount of data stored here */
+    unsinged int access_key;    /* used by sculluid and scullpriv */
+    struct semaphore sem;       /* mutual exclusion semaphore */
+    struct cdev cdev;            /* Char device struct */
+};
+
 #define SCULL_MAJOR 0
 #define SCULL_MINOR 0
 #define SCULL_NR_DEVS 1
+
+#ifndef SCULL_QUANTUM
+#define SCULL_QUANTUM 4000
+#endif
+
+#ifndef SCULL_QSET
+#define SCULL_QSET    1000
+#endif
+
 #endif
