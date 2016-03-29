9cc46bf6a244102fd2af04b25b501caddd0bc7d7
iourigordon <iouri.gordon@gmail.com>
2016-03-28 21:44:19 -0700

    major/minor version registration

--- airport_module/airport.c
+++ airport_module/airport.c
@@ -1,17 +1,36 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+
+
+#define AIRPORT_MAJOR 39
+
+enum {
+    AIRPORT_HANGAR,
+    AIRPORT_LAND_STRIP,
+    AIRPORT_TAKEOFF_STRIP,
+    AIRPORT_MAX
+};
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+static dev_t device;
+
 static int airport_init(void)
 {
-    printk(KERN_ALERT "Hello World\n");
+
+    device = MKDEV(AIRPORT_MAJOR,0);
+    if (register_chrdev_region(device,1,"airport")) {
+        printk(KERN_ERR "Failed to get chrdev region");
+    }
     return 0;
 }
 
 static void airport_exit(void)
 {
-    printk(KERN_ALERT "Goodbye\n");
+    printk(KERN_ALERT "Unregistering airport devices\n");
+    unregister_chrdev_region(device,1); 
 }
 
 module_init(airport_init);
