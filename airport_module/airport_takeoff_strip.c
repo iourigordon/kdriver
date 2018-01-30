7d04dbad7b77d92d5eb33f577f3a47d83feb19a5
Iouri Gordon <iouri.gordon@gmail.com>
2018-01-29 19:42:27 -0800

    airport_land_strip.c
    changes

--- airport_module/airport_takeoff_strip.c
+++ airport_module/airport_takeoff_strip.c
@@ -1,5 +1,24 @@
 #include <linux/fs.h>
 
+#define MAX_TAKEOFF_STRIP_RUNWAYS 2
+
 struct file_operations airport_takeoff_strip_ops = {
     .owner = THIS_MODULE
 };
+
+struct _takeoff_strip {
+    int max_planes;
+    atomic_t available_runways;
+    struct cdev cdev;
+};
+
+static struct _takeoff_strip takeoff_strip;
+
+int takeoff_strip_open(stuct inode *inode, struct file *filep)
+{
+    struc _takeoff_strip *takeoff_stripi_dev;
+
+    printk(KERN_INFO "Plane %d ready for take off\n",current->pid);
+    
+    
+}
